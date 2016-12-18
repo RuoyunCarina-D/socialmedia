81c97f27e2059fd3f83c4fea7ee6d170a37e4aae
Ruoyun Carina Deng <carina.deng@outlook.com>
2016-12-18 02:33:28 -0500

    Update twitter_vis.R

--- twitter_vis.R
+++ twitter_vis.R
@@ -1,6 +1,11 @@
+library(wordcould2)
+library(tm)
+
+#import twitter text file and transfer, then calculate the frequency of each word
 twitter<- scan("/Users/RuoyunDeng/Documents/Master/social_media/twitter_text.txt", what="character", sep="\n")
 twitter_np <- gsub( "[^[:alnum:]@]", " ", twitter)
 myCorpus <- Corpus(VectorSource(twitter_np))
+
 myTdm <- TermDocumentMatrix(myCorpus)
 temp <- inspect(myTdm)
 FreqMat <- data.frame(ST = rownames(temp), Freq = rowSums(temp))
@@ -8,12 +13,15 @@ row.names(FreqMat) <- NULL
 FreqMat_np <- FreqMat
 FreqMat
 
-
-
+#use wordcloud2 to do a initially visualization 
 wordcloud2(data=FreqMat, minSize = 8)
-#delet the most frequency two "the" and "gwu"
 
+#seperate users involving in those tweets
+n_at <- grep("@", FreqMat_np$ST)
+name_list <- FreqMat_np[n_at,]
+FreqMat_np_nn <- FreqMat_np[-n_at,]
 
+#delet the most frequency but meaningless words such as "the"
 n_the <- grep(382, FreqMat$Freq) #the
 n_gwu <- grep(788, FreqMat$Freq)
 n_amp <- grep(213, FreqMat$Freq)
@@ -24,13 +32,6 @@ n_and <- grep(125, FreqMat$Freq)
 FreqMat_np <- FreqMat[-c(n_the, n_gwu,n_amp,n_for,n_https,n_and),]
 wordcloud2(data=FreqMat_np)
 
-#@
-n_at <- grep("@", FreqMat_np$ST)
-name_list <- FreqMat_np[n_at,]
-FreqMat_np_nn <- FreqMat_np[-n_at,]
-
+#final pic
 wordcloud2(data=FreqMat_np_nn)
-
-sort_Fre <- sort(FreqMat_np_nn, decreasing = T )
-
 
