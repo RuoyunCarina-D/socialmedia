f7d2b0927b45704d2efc917a9c5b6dfd7d8058bb
Ruoyun Carina Deng <carina.deng@outlook.com>
2017-02-24 16:55:07 -0500

    Update facebookAPI.R

--- facebookAPI.R
+++ facebookAPI.R
@@ -2,17 +2,18 @@ library(devtools)
 install_github("Rfacebook", "pablobarbera", subdir="Rfacebook")
 require (Rfacebook)
 
-app_id = "254348755008159"
-app_secret = "a95d5c129547bedcc6de93c1a39b6b06"
+app_id = "***"
+app_secret = "***"
 fb_oauth <- fbOAuth(app_id=app_id, app_secret=app_secret,extended_permissions = TRUE)
 
 me <- getUsers("me",token=fb_oauth)
 my_likes <- getLikes(user="me", token=fb_oauth)
 
-token = "EAACEdEose0cBAHNydNnT4uSstESrRzj8CSsI3q8yGdi8R9AFbO0qhUz3NXt52czoPmGpsXkoE0P5BcbClHyAvSe6AxZAymc4ee7D2P6al3PLcvIKflBecYRkh9WClwOSWoFR7IcEKeMPZCNhVqTo48oIEZAcS9IBKTaZCzpSkngv1QdC8EcJzSw5tnsorQMZD"
+token = "***"
 me <- getUsers("me", token, private_info=TRUE)
 getUsers(c("barackobama", "donaldtrump"), token)
 
+# Sad news: Facebook now can only fetch information of friends who had used development app rather than all friends
 my_friends <- getFriends(token, simplify = F)
 head(my_friends$id, n = 1)
 my_friends_info <- getUsers(my_friends$id, token, private_info = TRUE)
